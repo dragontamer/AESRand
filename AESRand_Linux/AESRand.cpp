425fa20f6bdabc5c2616e5c01738fdec0f4a1081
Dragontamer <3312438+dragontamer@users.noreply.github.com>
2018-11-06 22:02:40 -0500

    Adding much simplified Linux code, additional tricks are documented in the Windows version

---
+++ AESRand_Linux/AESRand.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <immintrin.h>
+#include <array>
+
+
+__m128i AESRand_init(){
+	return _mm_setzero_si128(); 
+}
+
+__m128i increment = _mm_set_epi8(0x2f, 0x2b, 0x29, 0x25, 0x1f, 0x1d, 0x17, 0x13, 
+		0x11, 0x0D, 0x0B, 0x07, 0x05, 0x03, 0x02, 0x01); 
+
+void AESRand_increment(__m128i& state){
+	state += increment; 
+}
+
+struct TwoResults{
+	__m128i high;
+	__m128i low;
+};
+
+TwoResults AESRand_rand(const __m128i state){
+	__m128i penultimate = _mm_aesenc_si128(state, increment); 
+	return {_mm_aesenc_si128(penultimate, increment), _mm_aesdec_si128(penultimate, increment)};
+}
+
+int main(){
+	std::cout << "Running 5-billion Randoms" << std::endl; 
+	__m128i state = AESRand_init(); 
+	__m128i total = _mm_setzero_si128(); 
+	__m128i total2 = _mm_setzero_si128(); 
+
+	for(long long i=0; i<5000000000; i++){
+		AESRand_increment(state); 
+		auto rands = AESRand_rand(state); 
+		total += rands.high;
+		total2 += rands.low; 
+	}
+
+	total += total2;
+	std::cout << "Dummy print to negate optimizer: " << total[0] << std::endl;
+}
